a6f531b44ec9bd3b21d8f1c93d9378c2d8d779c1
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-11-04 20:48:35 -0700

    init

---
+++ objects/explosives/bomb.gd
@@ -0,0 +1,72 @@
+extends RigidBody2D
+
+const EXPLOSION_FORCE : int = 1500
+const RANGE_SIZE : float = 128
+const ANGLE_FORGIVENESS : float = 0.2
+
+const explosion_fx := preload("res://objects/explosives/explosionfx.tscn")
+
+@onready var aoe: Area2D = $Aoe
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func start_countdown(from: int) -> void:
+	var count := from
+	if count <= 0:
+		for _i in 3:
+			await get_tree().process_frame
+		explode()
+		return
+	while count > 0:
+		$Label.text = str(count)
+		await get_tree().create_timer(1.0).timeout
+		count -= 1
+	explode()
+
+
+func explode() -> void:
+	collision_layer = 0
+	var objects := aoe.get_overlapping_bodies()
+	animation_player.play(&"destroy")
+	var efx := explosion_fx.instantiate()
+	efx.global_position = global_position
+	get_tree().current_scene.add_child(efx)
+	push_away_objects(objects)
+	destroy_objects(objects)
+	queue_free()
+
+
+func push_away_objects(objects: Array[Node2D]) -> void:
+	print("running")
+	for object in objects:
+		if not is_instance_valid(object):
+			continue
+		print(object)
+		if not object is RigidBody2D:
+			continue
+		
+		prints("hitting", object)
+		var force_dir : Vector2 = -(global_position - object.global_position).normalized()
+		var angle_check : float = -get_angle_to(object.global_position)
+		if angle_check > PI / 2 - ANGLE_FORGIVENESS and angle_check < PI / 2 + ANGLE_FORGIVENESS:
+			force_dir = Vector2.UP
+			print("corrected angle!")
+		prints("angle", angle_check)
+		var force_size: Vector2 = force_dir \
+		* cubic_interpolate(
+			0.3, 0.9, 0, 1.0,
+			1.0 - clampf(abs(global_position.distance_to(object.global_position)) / RANGE_SIZE, 0, 1)
+			)\
+		* EXPLOSION_FORCE
+		
+		object.apply_impulse(force_size)
+	print("ran")
+	await get_tree().physics_frame
+
+
+func destroy_objects(objects: Array[Node2D]) -> void:
+	for object in objects:
+		if not is_instance_valid(object):
+			continue
+		if not object.has_method(&"destroy"):
+			continue
+		object.destroy()
---
+++ tests/explosiontest/explosiontest.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+const bombscene := preload("res://objects/explosives/bomb.tscn")
+
+var timer_value : int = 1
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			match event.button_index:
+				MOUSE_BUTTON_LEFT:
+					spawnbomb(timer_value)
+				MOUSE_BUTTON_RIGHT:
+					spawnbomb(0)
+				MOUSE_BUTTON_WHEEL_DOWN:
+					timer_value -= 1
+					if timer_value < 1: timer_value = 1
+				MOUSE_BUTTON_WHEEL_UP:
+					timer_value += 1
+					if timer_value > 10: timer_value = 10
+
+
+func spawnbomb(timer : int) -> void:
+	var b := bombscene.instantiate()
+#	await b.ready
+	b.global_position = get_global_mouse_position()
+	add_child(b)
+	b.start_countdown(timer)
