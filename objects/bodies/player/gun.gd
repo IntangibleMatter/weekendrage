7c54ed81253d5654b63a8b06a4edb0a095a6afbe
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-11-06 14:37:43 -0800

    chatGPT really doesn't get it huh

--- objects/bodies/player/gun.gd
+++ objects/bodies/player/gun.gd
@@ -19,12 +19,33 @@ func fire() -> void:
 	if active_bombs >= max_bomb_count:
 		return
 	var b := bombscene.instantiate()
-	b.apply_impulse(muzzle.global_position.direction_to(get_global_mouse_position()) * bomb_start_velocity)
+#	b.apply_impulse(muzzle.global_position.direction_to(get_global_mouse_position()) * bomb_start_velocity)
+	b.apply_impulse(calculate_bomb_velocity())
 	b.global_position = muzzle.global_position
 	b.exploded.connect(update_bomb_count)
 	active_bombs += 1
 	frame = active_bombs
 	get_tree().current_scene.add_child(b)
+
+
+func calculate_bomb_velocity() -> Vector2:
+	var muzzpos : Vector2 = muzzle.global_position
+	var mousepos : Vector2 = get_global_mouse_position()
+	var diff : Vector2 = Vector2(muzzpos.x - mousepos.x, muzzpos.y - mousepos.y)
+
+	var launch_angle := atan2(-2 * diff.y, diff.x)
+
+	var init_velocity : Vector2 = Vector2(
+		diff.x / (2 * cos(launch_angle)),
+		(diff.x * tan(launch_angle) - diff.y) /
+		(2 * diff.x / (ProjectSettings.get_setting("physics/2d/default_gravity") * cos(launch_angle)))
+	)
+
+	var vel := sqrt(init_velocity.x * init_velocity.x + init_velocity.y * init_velocity.y)
+	prints("initial velocity:", vel)
+	prints("angle:", launch_angle)
+
+	return Vector2.RIGHT.rotated(launch_angle) * vel
 
 
 func _process(_delta: float) -> void:
@@ -37,4 +58,3 @@ func update_bomb_count() -> void:
 	active_bombs = get_tree().get_nodes_in_group(&"playerbomb").size() - 1
 	print(active_bombs)
 	frame = active_bombs
-
